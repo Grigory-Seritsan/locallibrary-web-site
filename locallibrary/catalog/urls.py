375c639dfa80db7fe78385c1a127aa5bf412acdf
Grigory_Seritsan <5tud3n@gmail.com>
2022-07-11 00:05:00 +0300

    1. bag fix incorrect name for css file 2. add new url patterns for pages All books and All authors 3.                    - for book detail and author detail 4. add views for new page 5. create templates for new page

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -1,7 +1,11 @@
-from django.urls import path
+from django.urls import re_path as url 
 from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    url(r'^$', views.index, name='index'),
+    url(r'^books/$', views.BookListView.as_view(), name = 'books'),
+    url(r'^books/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
+    url(r'^authors/$', views.AuthorListView.as_view(), name='authors'),
+    url(r'^authors/(?P<pk>\d+)$', views.AuthorDetailView.as_view(), name='author-detail')
 ]
