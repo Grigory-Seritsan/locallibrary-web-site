fd7b5c927c4e873ca836fa43ad85207a88b5653a
Grigory_Seritsan <5tud3n@gmail.com>
2022-07-11 21:50:54 +0300

    append num_visits

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -13,6 +13,8 @@ def index(request):
     # Доступные книги (статус = 'a')
     num_instances_available=BookInstance.objects.filter(status__exact='a').count()
     num_authors=Author.objects.count()  # Метод 'all()' применён по умолчанию.
+    num_visits=request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits+1
     num_genre = Genre.objects.all().count()
     num_anna = Book.objects.filter(title__contains = 'anna').count()
 
@@ -22,7 +24,8 @@ def index(request):
     return render(
         request,
         'index.html',
-        context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors, 'num_genre':num_genre, 'num_anna':num_anna},
+        context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors, 'num_genre':num_genre, 'num_anna':num_anna,
+        'num_visits':num_visits},
     )
 
 from django.views import generic
